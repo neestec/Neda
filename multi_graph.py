fca6a478133e8976a3ac726d4ecc788f3fa8b164
neestec <71190355+neestec@users.noreply.github.com>
2022-04-30 23:29:04 +0430

    Add files via upload

--- multi_graph.py
+++ multi_graph.py
@@ -462,50 +462,45 @@ def weight_def (main_matrix):
     for i in range(Total_Node):
         for j in range(Total_Node):
             node1 = []
-            node2 = []
-
             if main_matrix[i][j] ==1:
-                internal_pair = []
-                internal_pair.append(i)
-                internal_pair.append(j)
-
-                if internal_pair in pair:
-                    is_in = True
-                else:
-                    pair.append(internal_pair)
-                    print('internal_pair:' , internal_pair , 'pair:' , pair)
-                    weight = np.random.randint(1, 20)
-                    node1.append(i)
-                    node1.append(j)
-                    node1.append(weight)
-                    list_of_weight.append(node1)
-                    node2.append(j)
-                    node2.append(i)
-                    node2.append(weight)
-                    list_of_weight.append(node2)
-
-
+                weight = np.random.randint(1, 20)
+                node1.append(i)
+                node1.append(j)
+                node1.append(weight)
+                list_of_weight.append(node1)
     print('list of weight :: ' , list_of_weight)
-    return list_of_weight
+    for node in list_of_weight:
+        i = node[0]
+        j = node[1]
+        for n in list_of_weight:
+            if n[0] == j and n[1] == i:
+                n[2] = node[2]
+    print('list of weight correct:: ' , list_of_weight)
+    active_node_list = []
+    for node in list_of_weight:
+        if node[0] not in active_node_list:
+            active_node_list.append(node[0])
+    print('active_node: ', active_node_list)
+    return list_of_weight , active_node_list
 
-def weight_account(list_of_weight):
-
-    count = 0
-    weight_sum = 0
+def weight_account(list_of_weight , active_nodes ):
     node_and_weights = []
-    for node_number in range(Total_Node):
-        internal_node_and_weight = []
+    for node_number in active_nodes:
         for node in list_of_weight:
+            internal_node_and_weight = []
+            count = 0
+            weight_sum = 0
             if node_number == node[0]:
+                print('//////////////////////')
                 count = count+1
                 weight_sum = weight_sum+ node[2]
-            print('node_number ', node_number , 'node ' , node , 'count:', count , 'weight_sum: ', weight_sum)
+            print('node_number ', node_number ,'node[0]' , node[0] , 'node ',  node , 'count:', count , 'weight_sum: ', weight_sum)
             weight_avr = (weight_sum/count)
+
         internal_node_and_weight.append(node_number)
         internal_node_and_weight.append(weight_avr)
         node_and_weights.append(internal_node_and_weight)
-        count = 0
-        weight_sum = 0
+
     print ('node and weights: ' , node_and_weights)
 
 
@@ -533,8 +528,8 @@ Main_Graph = show_main_graph(Main_Matrix, Label)
 #closeness_recursive_dis(2, Main_Matrix)
 #random_recursive_dis(Main_Matrix)
 
-List_of_Weight = weight_def (Main_Matrix)
-#weight_account(List_of_Weight)
+List_of_Weight , Active_Nodes = weight_def (Main_Matrix)
+weight_account(List_of_Weight, Active_Nodes)
 
 
 
